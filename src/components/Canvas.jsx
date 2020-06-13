b338491a21521f55f2c6249d17f8fbd22aaa92e4
northross <rossnorthrup@yahoo.com>
2020-06-13 11:11:33 -0400

    Sky.jsx was added and exported into canvas app.js was refactored

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import Sky from './Sky';
 
 const Canvas = () => {
-  const style = {
-    border: '1px solid black',
-  };
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
-      style={style}
+      viewBox={viewBox}
     >
+      <Sky />
       <circle cx={0} cy={0} r={50} />
     </svg>
   );
