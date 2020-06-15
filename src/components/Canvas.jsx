8f70de362009d09aea051a89be9b3c7a7469d3b6
northross <rossnorthrup@yahoo.com>
2020-06-15 11:48:20 -0400

    added score to canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import Ground from './Ground'
 import CannonBase from './CannonBase'
 import CannonPipe from './CannonPipe'
 import CannonBall from './CannonBall'
+import CurrentScore from './CurrentScore'
 import PropTypes from 'prop-types'
 
 const Canvas = (props) => {
@@ -16,12 +17,17 @@ const Canvas = (props) => {
     onMouseMove={props.trackMouse}
     viewBox={viewBox}
   >
-
+    <defs>
+      <filter id="shadow">
+        <feDropShadow dx="1" dy="1" stdDeviation="2" />
+      </filter>
+    </defs>
     <Sky />
     <Ground />
     <CannonPipe rotation={props.angle} />
     <CannonBase />
     <CannonBall position={{x: 0, y: -100}}/>
+    <CurrentScore score={15} />
   </svg>
   )
 }
