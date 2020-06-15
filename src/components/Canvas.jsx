4a7292efececb02fa2f76b8ff6ced4d2bafefe47
northross <rossnorthrup@yahoo.com>
2020-06-15 12:38:15 -0400

    made game reactive to click and adjusted games states

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,50 +1,61 @@
-import React from 'react'
-import Sky from './Sky'
-import Ground from './Ground'
-import CannonBase from './CannonBase'
-import CannonPipe from './CannonPipe'
-import CannonBall from './CannonBall'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sky from './Sky';
+import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
 import CurrentScore from './CurrentScore'
-import FlyingObject from './FlyingObject'
-import Heart from './Heart'
-import StartGame from './StartGame'
-import Title from './Title'
-import PropTypes from 'prop-types'
+import FlyingObject from './FlyingObject';
+import StartGame from './StartGame';
+import Title from './Title';
 
 const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
-
   return (
-  <svg
-    id="aliens-go-home-canvas"
-    preserveAspectRatio="xMaxYMax none"
-    onMouseMove={props.trackMouse}
-    viewBox={viewBox}
-  >
-    <defs>
-      <filter id="shadow">
-        <feDropShadow dx="1" dy="1" stdDeviation="2" />
-      </filter>
-    </defs>
-    <Sky />
-    <Ground />
-    <CannonPipe rotation={props.angle} />
-    <CannonBase />
-    <CannonBall position={{x: 0, y: -100}}/>
-    <CurrentScore score={15} />
-    <FlyingObject position={{x: -150, y: -300}}/>
-    <FlyingObject position={{x: 150, y: -300}}/>
-    <Heart position={{x: -300, y: 35}}/>
-    <StartGame onClick={() => console.log('Aliens, Go Home!')} />
-    <Title />
-  </svg>
-  )
-}
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+    >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CurrentScore score={15} />
+
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      }
+
+      { props.gameState.started &&
+        <g>
+          <FlyingObject position={{x: -150, y: -300}}/>
+          <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }
+    </svg>
+  );
+};
 
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
-  trackMouse: PropTypes.func.isRequired
-}
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
+};
 
-export default Canvas
+export default Canvas;
