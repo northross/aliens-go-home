2d17d7b1f0a61dbed7a98adf7d4a4fc250428478
northross <rossnorthrup@yahoo.com>
2020-06-22 14:57:15 -0400

    Updates

---
+++ precache-manifest.c51fa4ecda677621f4814dce58f1f4a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abf8daec22a049c87cd00543dcc4187",
+    "url": "/aliens-go-home/index.html"
+  },
+  {
+    "revision": "4d4108d3594d3c5cabb1",
+    "url": "/aliens-go-home/static/css/main.535dceb8.chunk.css"
+  },
+  {
+    "revision": "6cc73e96be90229f1524",
+    "url": "/aliens-go-home/static/js/2.1583177a.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/aliens-go-home/static/js/2.1583177a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4108d3594d3c5cabb1",
+    "url": "/aliens-go-home/static/js/main.5691560c.chunk.js"
+  },
+  {
+    "revision": "017a44ab17f2286025e5",
+    "url": "/aliens-go-home/static/js/runtime-main.ca0d8a37.js"
+  }
+]);
