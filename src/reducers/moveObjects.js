aee9eab4f4f86ceebf9c1bc30621ac8290f008bf
northross <rossnorthrup@yahoo.com>
2020-06-22 13:41:50 -0400

    lives work, shooting works, score updates

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -41,6 +41,8 @@ function moveObjects(state, action) {
   cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
   flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
 
+  const kills = state.gameState.kills + flyingDiscsDestroyed.length
+
   return {
     ...newState,
     gameState: {
@@ -49,6 +51,7 @@ function moveObjects(state, action) {
       cannonBalls: [...cannonBalls],
       lives,
       started,
+      kills,
     },
     angle,
   };
