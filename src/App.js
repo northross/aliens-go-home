079a24dea477aadb67ee0ea2db9bacaada56d6d7
northross <rossnorthrup@yahoo.com>
2020-06-22 13:01:31 -0400

    Added shoot funcitonatlity cannonballs can move and be shot

--- src/App.js
+++ src/App.js
@@ -1,51 +1,89 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Canvas from './components/Canvas'
-import { getCanvasPosition } from './utils/formulas';
-import * as Auth0 from 'auth0-web';
-
+import { getCanvasPosition } from './utils/formulas'
+import * as Auth0 from 'auth0-web'
+import io from 'socket.io-client'
 Auth0.configure({
-  domain: 'YOUR_AUTH0_DOMAIN',
-  clientID: 'YOUR_AUTH0_CLIENT_ID',
+  domain: 'dev-ross.us.auth0.com',
+  clientID: 'GD4ueaEZ6mYdBNUTmjZv0OCWZfTq6RMp',
   redirectUri: 'http://localhost:3000/',
   responseType: 'token id_token',
   scope: 'openid profile manage:points',
-});
-
+  audience: 'https://aliens-go-home.digituz.com.br',
+})
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.shoot = this.shoot.bind(this)
+  }
   componentDidMount() {
     const self = this
-    Auth0.handleAuthCallback();
+    Auth0.handleAuthCallback()
     Auth0.subscribe((auth) => {
-      console.log(auth);
-    });
+      if (!auth) return
+      const playerProfile = Auth0.getProfile()
+      const currentPlayer = {
+        id: playerProfile.sub,
+        maxScore: 0,
+        name: playerProfile.name,
+        picture: playerProfile.picture
+      }
+      this.props.loggedIn(currentPlayer)
+      const socket = io('http://localhost:3001', {
+        query: `token=${Auth0.getAccessToken()}`
+      })
+      let emitted = false
+      socket.on('players', (players) => {
+        this.props.leaderboardLoaded(players)
+        if (emitted) return
+        socket.emit('new-max-score', {
+          id: playerProfile.sub,
+          maxScore: 120,
+          name: playerProfile.name,
+          picture: playerProfile.picture
+        })
+        emitted = true
+        setTimeout(() => {
+          socket.emit('new-max-score', {
+            id: playerProfile.sub,
+            maxScore: 222,
+            name: playerProfile.name,
+            picture: playerProfile.picture
+          })
+        }, 5000)
+      })
+    })
     setInterval(() => {
       self.props.moveObjects(self.canvasMousePosition)
     }, 10)
-    window.onresize = () => {
-      const cnv = document.getElementById('aliens-go-home-canvas');
-      cnv.style.width = `${window.innerWidth}px`;
-      cnv.style.height = `${window.innerHeight}px`;
-    };
-    window.onresize();
+      window.onresize = () => {
+    const cnv = document.getElementById('aliens-go-home-canvas')
+    cnv.style.width = `${window.innerWidth}px`
+    cnv.style.height = `${window.innerHeight}px`
+    }
+    window.onresize()
   }
-
   trackMouse(event) {
     this.canvasMousePosition = getCanvasPosition(event)
   }
-
+  shoot() {
+    this.props.shoot(this.canvasMousePosition);
+  }
   render() {
     return (
       <Canvas
         angle={this.props.angle}
+        currentPlayer={this.props.currentPlayer}
         gameState={this.props.gameState}
+        players={this.props.players}
         startGame={this.props.startGame}
         trackMouse={event => (this.trackMouse(event))}
+        shoot={this.shoot}
       />
     )
   }
 }
-
 App.propTypes = {
   angle: PropTypes.number.isRequired,
   gameState: PropTypes.shape({
@@ -59,11 +97,27 @@ App.propTypes = {
       }).isRequired,
       id: PropTypes.number.isRequired,
     })).isRequired,
+      moveObjects: PropTypes.func.isRequired,
   }).isRequired,
-  moveObjects: PropTypes.func.isRequired,
   startGame: PropTypes.func.isRequired,
-  shoot: PropTypes.func.isRequired,
-};
-
-
-export default App;
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  leaderboardLoaded: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  })),
+  shoot: PropTypes.func.isRequired
+}
+App.defaultProps = {
+  currentPlayer: null,
+  players: null,
+}
+export default App
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,41 @@
-import { MOVE_OBJECTS, START_GAME } from '../actions';
-import moveObjects from './moveObjects';
-import startGame from './startGame';
-
+import { LEADERBOARD_LOADED, LOGGED_IN, MOVE_OBJECTS, START_GAME, SHOOT } from '../actions'
+import moveObjects from './moveObjects'
+import startGame from './startGame'
+import shoot from './shoot'
 const initialGameState = {
   started: false,
   kills: 0,
   lives: 3,
   flyingObjects: [],
   lastObjectCreatedAt: new Date(),
-};
-
+  currentPlayer: null,
+  players: null,
+  cannonBalls: []
+}
 const initialState = {
   angle: 45,
-  gameState: initialGameState,
-};
-
+  gameState: initialGameState
+}
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case LEADERBOARD_LOADED:
+      return {
+        ...state,
+        players: action.players,
+      }
+    case LOGGED_IN:
+      return {
+        ...state,
+        currentPlayer: action.player,
+      }
     case MOVE_OBJECTS:
-      return moveObjects(state, action);
+      return moveObjects(state, action)
     case START_GAME:
-      return startGame(state, initialGameState);
+      return startGame(state, initialGameState)
+    case SHOOT:
+      return shoot(state, action)
     default:
-      return state;
+      return state
   }
 }
-
-export default reducer;
+export default reducer
